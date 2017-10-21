dbf427ecd3941700578102390b1f542ccf3f77f9
varunzxzx <varun995862@gmail.com>
2017-10-21 16:12:48 +0530

    used Material ui Grid

--- src/components/Dashboard/dashboard.jsx
+++ src/components/Dashboard/dashboard.jsx
@@ -6,6 +6,11 @@ import Typography from 'material-ui/Typography';
 import List, { ListItem, ListItemText } from 'material-ui/List';
 import Avatar from 'material-ui/Avatar';
 import ReorderIcon from 'material-ui-icons/Reorder';
+import RankIcon from 'material-ui-icons/SupervisorAccount';
+import AverageIcon from 'material-ui-icons/Equalizer';
+import PlayedIcon from 'material-ui-icons/Games';
+import FunctionIcon from 'material-ui-icons/Functions';
+import Grid from 'material-ui/Grid';
 
 class Dashboard extends Component {
   constructor(props) {
@@ -44,7 +49,7 @@ class Dashboard extends Component {
         {
           title: "C++ Tricks",
           link: "http://codeforces.com/blog/entry/15643",
-          source: "Code Forces"
+          source: "CodeForces"
         }
       ],
       pieData:{
@@ -61,13 +66,13 @@ class Dashboard extends Component {
               95072
             ],
             backgroundColor:[
-              'rgba(255, 99, 132, 0.6)',
-              'rgba(54, 162, 235, 0.6)',
-              'rgba(255, 206, 86, 0.6)',
-              'rgba(75, 192, 192, 0.6)',
-              'rgba(153, 102, 255, 0.6)',
-              'rgba(255, 159, 64, 0.6)',
-              'rgba(255, 99, 132, 0.6)'
+              'rgba(255, 99, 132, 0.9)',
+              'rgba(54, 162, 235, 0.9)',
+              'rgba(255, 206, 86, 0.9)',
+              'rgba(75, 192, 192, 0.9)',
+              'rgba(153, 102, 255, 0.9)',
+              'rgba(255, 159, 64, 0.9)',
+              'rgba(255, 206, 132, 0.9)'
             ]
           }
         ]
@@ -77,19 +82,22 @@ class Dashboard extends Component {
 
   render() {
     const styles = {
-      item: {
-        textAlign: "center",
-        margin: "10px auto",
-        padding: "10px"
-      },
       inner: {
         padding: "15px 0px 10px 15px",
         textAlign: "left",
-        height: "80px"
+        height: "80px",
+      },
+      transparent: {
+        position: "relative",
+        width: "60px",
+        height: "60px",
+        bottom: "60px",
+        right: "-220px",
+        fillOpacity: "0.6"
       }
     }
     return(
-      <div id="dash">
+      <div className="dash">
           <Card style={{width: "95%",margin: "auto"}}>
             <CardContent>
               <Line
@@ -101,92 +109,97 @@ class Dashboard extends Component {
               />
             </CardContent>
           </Card>
-          <div className="row" style={{width: "95%",margin: "auto"}}>
-            <div className="col-md-3 col-sm-12" style={{...styles.item,paddingLeft: "0px"}}>
-              <div style={{...styles.inner,background: "#FF7043",borderBottom: "5px solid #BF360C"}}>
-                <Typography type="headline" style={{marginBottom: "2px",fontSize: "30px"}} gutterBottom>
-                  16
-                </Typography>
-                <Typography type="body1" style={{fontSize: "16px"}} gutterBottom>
-                  Overall Rank
-                </Typography>
-              </div>
-            </div>
-            <div className="col-md-3 col-sm-12" style={styles.item}>
-              <div style={{...styles.inner,background: "#9FA8DA",borderBottom: "5px solid #3949AB"}}>
-                <Typography type="headline" style={{marginBottom: "2px",fontSize: "30px"}} gutterBottom>
-                  42.32%
-                </Typography>
-                <Typography type="body1" style={{fontSize: "16px"}} gutterBottom>
-                  Average Score
-                </Typography>
-              </div>
-            </div>
-            <div className="col-md-3 col-sm-12" style={styles.item}>
-              <div style={{...styles.inner,background: "#66BB6A",borderBottom: "5px solid #1B5E20"}}>
-                <Typography type="headline" style={{marginBottom: "2px",fontSize: "30px"}} gutterBottom>
-                  24
-                </Typography>
-                <Typography type="body1" style={{fontSize: "16px"}} gutterBottom>
-                  Questions Attempted
-                </Typography>
-              </div>
-            </div>
-            <div className="col-md-3 col-sm-12" style={{...styles.item,paddingRight: "0px"}}>
-              <div style={{...styles.inner,background: "#FFC107",borderBottom: "5px solid #FF6F00"}}>
-                <Typography type="headline" style={{marginBottom: "2px",fontSize: "30px"}} gutterBottom>
-                  12
-                </Typography>
-                <Typography type="body1" style={{fontSize: "16px"}} gutterBottom>
-                  Quizzes Played
-                </Typography>
-              </div>
-            </div>
-          </div>
-          <div className="row" style={{width: "95%",margin: "auto"}}>
-            <div className="col-md-7 col-xs-12" style={{paddingLeft: "0px"}}>
-              <Card>
-                <CardContent>
-                  <Typography type="subheading" style={{fontWeight: "bold"}} gutterBottom>
-                    Recommended Reading
+          <div style={{flexGrow: "1",width: "95%",margin: " 15px auto 15px auto"}}>
+            <Grid container spacing={0}>
+                <Grid item md={3} xs={12} style={{...styles.inner,background:"#FF7043",borderBottom: "5px solid #BF360C"}}>
+                  <Typography type="headline" style={{marginBottom: "2px",fontSize: "30px"}} gutterBottom>
+                    16
                   </Typography>
-                  <List>
-                  {this.state.reading.map((read,i) => {
-                    return(
-                      <a key={i} style={{textDecoration: "none"}} href={read.link}>
-                        <ListItem button>
-                          <Avatar>
-                            <ReorderIcon />
-                          </Avatar>
-                          <ListItemText primary={read.title} secondary={read.source} />
-                        </ListItem>
-                      </a>
-                    )
-                  })}
-                  </List>
-                </CardContent>
-              </Card>
-            </div>
-            <div className="col-md-5 col-xs-12" style={{paddingRight: "0px"}}>
-              <Card>
-                <CardContent>
-                  <Pie
-                    data={this.state.pieData}
-                    options={{
-                      title:{
-                        display:true,
-                        text:'Overall Progress',
-                        fontSize:22
-                      },
-                      legend:{
-                        display:true,
-                        position:"right"
-                      }
-                    }}
-                  />
-                </CardContent>
-              </Card>
-            </div>
+                  <Typography type="body1" style={{fontFamily:"Source Sans Pro",fontSize: "16px"}} gutterBottom>
+                    Overall Rank
+                  </Typography>
+                  <RankIcon style={{...styles.transparent,color: "#BF360C"}}/>
+                </Grid>
+                <Grid item md={3} xs={12} style={{...styles.inner,background:"#9FA8DA",borderBottom: "5px solid #3949AB"}}>
+                  <Typography type="headline" style={{marginBottom: "2px",fontSize: "30px"}} gutterBottom>
+                    42.32%
+                  </Typography>
+                  <Typography type="body1" style={{fontFamily:"Source Sans Pro",fontSize: "16px"}} gutterBottom>
+                    Average Score
+                  </Typography>
+                  <AverageIcon style={{...styles.transparent,color: "#3949AB"}}/>
+                </Grid>
+                <Grid item md={3} xs={12} style={{...styles.inner,background:"#66BB6A",borderBottom: "5px solid #1B5E20"}}>
+                  <Typography type="headline" style={{marginBottom: "2px",fontSize: "30px"}} gutterBottom>
+                    24
+                  </Typography>
+                  <Typography type="body1" style={{fontFamily:"Source Sans Pro",fontSize: "16px"}} gutterBottom>
+                    Questions Attempted
+                  </Typography>
+                  <FunctionIcon style={{...styles.transparent,color: "#1B5E20"}}/>
+                </Grid>
+                <Grid item md={3} xs={12} style={{...styles.inner,background:"#FFC107",borderBottom: "5px solid #FF6F00"}}>
+                  <Typography type="headline" style={{marginBottom: "2px",fontSize: "30px"}} gutterBottom>
+                    12
+                  </Typography>
+                  <Typography type="body1" style={{fontFamily:"Source Sans Pro",fontSize: "16px"}} gutterBottom>
+                    Quizzes Played
+                  </Typography>
+                  <PlayedIcon style={{...styles.transparent,color: "#FF6F00"}}/>
+                </Grid>
+            </Grid>
+          </div>
+          <div style={{flexGrow: "1",width: "95%",margin: "auto"}}>
+            <Grid container spacing={24}>
+              <Grid item md={7} xs={12}>
+                <Card>
+                  <CardContent>
+                    <Typography type="subheading" style={{fontWeight: "bold"}} gutterBottom>
+                      Recommended Reading
+                    </Typography>
+                    <List>
+                    {this.state.reading.map((read,i) => {
+                      return(
+                        <a target="_blank" rel="nofollow" key={i} style={{textDecoration: "none"}} href={read.link}>
+                          <ListItem button>
+                            <Avatar>
+                              <ReorderIcon />
+                            </Avatar>
+                            <ListItemText primary={read.title} secondary={read.source} />
+                          </ListItem>
+                        </a>
+                      )
+                    })}
+                    </List>
+                  </CardContent>
+                </Card>
+              </Grid>
+              <Grid item md={5} xs={12}>
+                <Card style={{height: "290px"}}>
+                  <CardContent>
+                    <Typography type="subheading" style={{fontWeight: "bold"}} gutterBottom>
+                      Overall Progress
+                    </Typography>
+                    <Pie
+                      data={this.state.pieData}
+                      options={{
+                        title:{
+                          display:false,
+                          text:'Overall Progress',
+                          fontSize:16,
+                          fontFamily: "Roboto",
+                          fontColor: "black"
+                        },
+                        legend:{
+                          display:true,
+                          position:"right"
+                        }
+                      }}
+                    />
+                  </CardContent>
+                </Card>
+              </Grid>
+            </Grid>
           </div>
       </div>
     )
--- src/components/Statistics/statistics.jsx
+++ src/components/Statistics/statistics.jsx
@@ -1,17 +1,60 @@
 import React, { Component } from 'react';
 import NotFoundIcon from 'material-ui-icons/RoomService';
 import Typography from 'material-ui/Typography';
+import Grid from 'material-ui/Grid';
 
 class Statistics extends Component {
-  render() {
+  constructor(props) {
+    super(props);
+
+    this.state = {}
+    let width = window.innerWidth;
+    if (width > 768) {
+      this.state.renderComponent = this.desktop();
+     } else {
+       this.state.renderComponent = this.mobile();
+     }
+    window.addEventListener('resize', () => {
+      this.onresize();
+    }, true);
+  }
+
+  desktop = () => {
     return(
-      <div id="notfound">
-        <NotFoundIcon/>
-        <Typography type="display3" gutterBottom>
-          Coming Soon...
-        </Typography>
+      <div className="dash">
+        <div style={{flexGrow: "1",width: "95%", margin: "auto"}}>
+          <Grid container spacing={24}>
+            <Grid item xs={4}>
+              Hello, I am Left
+            </Grid>
+            <Grid item xs={8}>
+              Hello I am Right
+            </Grid>
+          </Grid>
+        </div>
       </div>
     )
+  }
+
+  mobile = () => {
+    return(
+      <div className="dash">
+        <h1>Hello, This is mobile</h1>
+      </div>
+    )
+  }
+
+  onresize = () => {
+    let width = window.innerWidth;
+    if (width > 768) {
+       this.setState({renderComponent: this.desktop()})
+     } else {
+       this.setState({renderComponent: this.mobile()})
+     }
+  }
+
+  render() {
+    return this.state.renderComponent;
   }
 }
 
